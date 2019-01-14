223fe707b6200fb44f2e2fe71dfc5d8a75785f5b
yankebin <ykbdevelop@gmail.com>
2019-01-14 13:35:49 +0800

    抽象进一步精简

--- unity3dplugin/src/main/java/com/ykbjson/lib/unity3dplugin/UnityPlayerActivity.java
+++ unity3dplugin/src/main/java/com/ykbjson/lib/unity3dplugin/UnityPlayerActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,12 +20,18 @@ import com.ykbjson.lib.unity3dplugin.internal.IOnUnity3DCall;
 import com.ykbjson.lib.unity3dplugin.internal.IUnityPlayerContainer;
 
 /**
- * Desription：Base Unity3D Player Activity
+ * Desription：Base Unity3D Player Activity.
+ * <BR/>
+ * If you want to implement Fragment to load the Unity scene, then the Fragment needs to refer to {@link UnityPlayerFragment} and {@link UnityPlayerFragmentV4}
+ *    To implement, then overload the {@link #generateIOnUnity3DCallDelegate(UnityPlayer, Bundle)} method to return the conforming Fragment.
+ * <BR/>
  * Creator：yankebin
+ * <BR/>
  * CreatedAt：2018/12/1
  */
 public abstract class UnityPlayerActivity extends BaseActivity implements IGetUnity3DCall, IOnUnity3DCall, IUnityPlayerContainer {
     protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+    protected IOnUnity3DCall mOnUnity3DCallDelegate;
 
     @Override
     @CallSuper
@@ -39,9 +46,25 @@ public abstract class UnityPlayerActivity extends BaseActivity implements IGetUn
      */
     protected void initUnityPlayer(@NonNull Bundle bundle) {
         mUnityPlayer = new UnityPlayer(this);
-        final ViewGroup unityContainer = findViewById(unityPlayerContainerId());
-        unityContainer.addView(mUnityPlayer);
-        mUnityPlayer.requestFocus();
+        mOnUnity3DCallDelegate = generateIOnUnity3DCallDelegate(mUnityPlayer, bundle);
+        if (null != mOnUnity3DCallDelegate) {
+            if (mOnUnity3DCallDelegate instanceof Fragment) {
+                getSupportFragmentManager().beginTransaction().replace(unityPlayerContainerId(),
+                        (Fragment) mOnUnity3DCallDelegate, ((Fragment) mOnUnity3DCallDelegate)
+                                .getClass().getName()).commit();
+            } else if (mOnUnity3DCallDelegate instanceof android.app.Fragment) {
+                getFragmentManager().beginTransaction().replace(unityPlayerContainerId(),
+                        (android.app.Fragment) mOnUnity3DCallDelegate, ((android.app.Fragment) mOnUnity3DCallDelegate)
+                                .getClass().getName()).commit();
+            } else {
+                throw new IllegalAccessError("Not a fragment");
+            }
+        } else {
+            mOnUnity3DCallDelegate = this;
+            final ViewGroup unityContainer = findViewById(unityPlayerContainerId());
+            unityContainer.addView(mUnityPlayer);
+            mUnityPlayer.requestFocus();
+        }
     }
 
     @Override
@@ -191,10 +214,17 @@ public abstract class UnityPlayerActivity extends BaseActivity implements IGetUn
         return this;
     }
 
-
     @NonNull
     @Override
     public IOnUnity3DCall getOnUnity3DCall() {
-        return this;
+        //Perhaps this method is called after Unity is created after the activity is created,
+        // so there is no problem for the time being.
+        return mOnUnity3DCallDelegate;
+    }
+
+    @Nullable
+    protected IOnUnity3DCall generateIOnUnity3DCallDelegate(@NonNull UnityPlayer unityPlayer,
+                                                            @Nullable Bundle bundle) {
+        return null;
     }
 }
